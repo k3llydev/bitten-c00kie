5cac6d51c037d48459350217bef0a802a1b0202e
Luis Kelly Diaz <luiskd_98@hotmail.com>
2019-04-18 16:31:55 -0500

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,24 +1,17 @@
 <?php
 
-    $pack = json_decode('["https:\/\/i.imgur.com\/iOSubga.jpg","https:\/\/i.imgur.com\/5o8NmFd.jpg","https:\/\/i.imgur.com\/V24AbGf.jpg","https:\/\/i.imgur.com\/wvHCAAy.jpg","https:\/\/i.imgur.com\/I61PVJR.jpg","https:\/\/i.imgur.com\/fRGbvQW.jpg","https:\/\/i.imgur.com\/DN7PB1a.jpg","https:\/\/i.imgur.com\/HfnbD5U.jpg","https:\/\/i.imgur.com\/wRoH9x8.jpg","https:\/\/i.imgur.com\/kE1t5W9.jpg","https:\/\/i.imgur.com\/Bd58zSX.jpg","https:\/\/i.imgur.com\/NCVzisD.jpg","https:\/\/i.imgur.com\/RgEjDGi.jpg","https:\/\/i.imgur.com\/eJUFLjj.jpg","https:\/\/i.imgur.com\/D2AlszA.jpg","https:\/\/i.imgur.com\/NDyjIeO.jpg","https:\/\/i.imgur.com\/fH6jXJz.jpg","https:\/\/i.imgur.com\/XKhm3Sy.jpg","https:\/\/i.imgur.com\/dlGBpWX.jpg","https:\/\/i.imgur.com\/s1TydeL.jpg","https:\/\/i.imgur.com\/CMky6Ot.jpg","https:\/\/i.imgur.com\/3cSBTq2.jpg","https:\/\/i.imgur.com\/bemXqik.jpg","https:\/\/i.imgur.com\/3js8sFn.jpg","https:\/\/i.imgur.com\/lFnWZ0Q.jpg","https:\/\/i.imgur.com\/9jEjwJE.jpg","https:\/\/i.imgur.com\/n5cDv7J.jpg","https:\/\/i.imgur.com\/s1E0sNK.jpg","https:\/\/i.imgur.com\/gV226Ze.jpg","https:\/\/i.imgur.com\/UuQBhrI.jpg","https:\/\/i.imgur.com\/dRmZQ4j.jpg","https:\/\/i.imgur.com\/JDo9vFg.jpg","https:\/\/i.imgur.com\/dxUoswo.jpg","https:\/\/i.imgur.com\/CyUgDAO.jpg"]');
+    $pack = []; // Array of images randomly used to show in the trick
 
 
-    $users =
+    $users = // Add users here, this stays private tho
         [
-            "martin" =>
+            "username" =>
                 [
-                    "key"=>"390023239",
-                    "user"=>"martin_vega",
-                    "pass"=>"isuzu98",
-                    "picture"=>"https://clanbb4l.com/wp-content/themes/blackfyre/img/defaults/default-profile.jpg"
-                ],
-            "k3dev" =>
-                [
-                    "key"=>"3",
-                    "user"=>"michelle",
-                    "pass"=>"xssposed",
-                    "picture"=>"http://graph.facebook.com/100001026874667/picture?width=500"
-                ],
+                    "key"=>"0123456789",
+                    "user"=>"example_user",
+                    "pass"=>"example_user_password",
+                    "picture"=>"picture_of_user"
+                ]
         ];
 
     function keyExists($key){
@@ -45,7 +38,7 @@
 
     function getUserPicture($user){
       global $users;
-      $pic = "https://clanbb4l.com/wp-content/themes/blackfyre/img/defaults/default-profile.jpg";
+      $pic = "default picture url";
       foreach($users as $ux){
         if($ux['user'] === $user){
           $pic = $ux['picture'];
